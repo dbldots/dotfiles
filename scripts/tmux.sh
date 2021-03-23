4e34f0edb2f944b5c9ca295ede4d39c9b815cfd4
Johannes-Kostas Goetzinger <johannes.goetzinger@mediafellows.com>
2021-03-23 11:22:08 +0100

    improve tmux script

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-wget https://raw.githubusercontent.com/dbldots/dotfiles/master/files/_tmux.conf -O ~/.johannes.tmux.conf
-tmux -L johannes new-session -d
-tmux -L johannes send-keys 'tmux source-file ~/.johannes.tmux.conf' C-j
-tmux -L johannes attach
+if ! tmux -L johannes attach ; then
+    wget https://raw.githubusercontent.com/dbldots/dotfiles/master/files/_tmux.conf -O ~/.johannes.tmux.conf
+    tmux -L johannes new-session -d
+    tmux -L johannes send-keys 'tmux source-file ~/.johannes.tmux.conf' C-j
+    tmux -L johannes attach
+fi
